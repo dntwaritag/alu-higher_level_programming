ff8292aa6d4cd0b5249eb3d56d0a52850b50b10e
Denys NTWARITAGANZWA <d.ntwaritag@alustudent.com>
2023-07-07 08:57:27 -0700

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Okeke Makuochukwu
+# Author - Denys NTWARITAGANZWA
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
