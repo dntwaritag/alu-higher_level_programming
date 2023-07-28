bc6a53ee4e00025fbd0a54ee1cd63aafbab3e51c
Denys NTWARITAGANZWA <d.ntwaritag@alustudent.com>
2023-07-28 01:51:18 -0700

    0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module: 0-rectangle
+This is the Rectangle module.
+It defines a simple rectangle without any attributes or methods.
+"""
+
+
+class Rectangle:
+    """
+    This is the Rectangle class.
+    It defines a simple rectangle without any attributes or methods.
+    """
+    pass
+
