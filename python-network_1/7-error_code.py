0ad515e1608518292b73ebdaed1346f0547f729e
Denys NTWARITAGANZWA <d.ntwaritag@alustudent.com>
2023-08-02 08:00:02 -0700

    7-error_code.py

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -3,10 +3,10 @@
 import requests
 import sys
 
-if __name__ == '__main__':
-    url = sys.argv[1]
-    response = requests.get(url)
-    if response.status_code >= 400:
-        print("Error code: {}".format(response.status_code))
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print("{}".format(response.text))
+        print(r.text)
