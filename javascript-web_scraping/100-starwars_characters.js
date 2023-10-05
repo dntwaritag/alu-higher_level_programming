b6369ae74a30f578d339c3570a4254bb972dfdd2
Denys NTWARITAGANZWA <d.ntwaritag@alustudent.com>
2023-10-05 11:36:21 -0700

    101-starwars_characters.js

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,46 +1,22 @@
 #!/usr/bin/node
-
 const request = require('request');
+const args = process.argv;
 
-if (process.argv.length > 2) {
-  const movieId = process.argv[2];
-  const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-  request.get(apiUrl, (error, response, body) => {
-    if (error) {
-      console.error(`An error occurred while making the request: ${error}`);
-      return;
-    }
-
-    if (response.statusCode !== 200) {
-      console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
-      return;
-    }
-
-    try {
-      const movie = JSON.parse(body);
-      const charactersUrls = movie.characters;
-
-      charactersUrls.forEach(characterUrl => {
-        request.get(characterUrl, (error, response, body) => {
-          if (error) {
-            console.error(`An error occurred while making the request: ${error}`);
-            return;
-          }
-
-          if (response.statusCode !== 200) {
-            console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
-            return;
-          }
-
-          const character = JSON.parse(body);
-          console.log(character.name);
-        });
+request(`https://swapi-api.hbtn.io/api/films/${args[2]}`, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const charEnd = data.characters;
+    const loopTime = charEnd.length;
+    for (let i = 0; i < loopTime; i++) {
+      request(charEnd[i], function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
       });
-    } catch (error) {
-      console.error(`An error occurred while parsing the response: ${error}`);
     }
-  });
-} else {
-  console.log('Please provide the Movie ID as an argument.');
-}
+  }
+});
